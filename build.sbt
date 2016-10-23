145325060b8633abde963ec91e6101046fba2410
Neville Li <neville@spotify.com>
2016-10-22 23:40:11 -0400

    add proto3test project

--- build.sbt
+++ build.sbt
@@ -1,14 +1,5 @@
 import sbtprotobuf.{ProtobufPlugin => PB}
 
-organization := "me.lyh"
-name := "protobuf-generic"
-description := "Generic protobuf manipulation"
-
-scalaVersion := "2.11.8"
-crossScalaVersions := Seq("2.10.6", "2.11.8")
-scalacOptions ++= Seq("-target:jvm-1.7", "-deprecation", "-feature", "-unchecked")
-javacOptions ++= Seq("-source", "1.7", "-target", "1.7", "-Xlint:unchecked")
-
 val protobufVersion = Option(sys.props("protobuf.version")).getOrElse("3.1.0")
 val protocVersion = Map("2.6.1" -> "-v261", "3.0.2" -> "-v310", "3.1.0" -> "-v310")(protobufVersion)
 
@@ -19,50 +10,92 @@ val paradiseVersion = "2.1.0"
 val scalaCheckVersion = "1.13.2"
 val scalaTestVersion = "3.0.0"
 
-libraryDependencies ++= Seq(
-  "com.google.protobuf" % "protobuf-java" % protobufVersion % "provided",
-  "com.google.code.findbugs" % "jsr305" % jsr305Version % "provided",
-  "com.google.guava" % "guava" % guavaVersion,
-  "com.fasterxml.jackson.module" %% "jackson-module-scala" % jacksonVersion,
-  "org.scalatest" %% "scalatest" % scalaTestVersion % "test"
+val commonSettings = PB.protobufSettings ++ Seq(
+  organization := "me.lyh",
+
+  scalaVersion := "2.11.8",
+  crossScalaVersions := Seq("2.10.6", "2.11.8"),
+  scalacOptions ++= Seq("-target:jvm-1.7", "-deprecation", "-feature", "-unchecked"),
+  javacOptions ++= Seq("-source", "1.7", "-target", "1.7", "-Xlint:unchecked"),
+
+  libraryDependencies ++= Seq(
+    "com.google.protobuf" % "protobuf-java" % protobufVersion % "provided",
+    "com.google.code.findbugs" % "jsr305" % jsr305Version % "provided",
+    "com.google.guava" % "guava" % guavaVersion,
+    "com.fasterxml.jackson.module" %% "jackson-module-scala" % jacksonVersion,
+    "org.scalatest" %% "scalatest" % scalaTestVersion % "test"
+  ),
+
+  libraryDependencies ++= (
+    if (scalaBinaryVersion.value == "2.10")
+      Seq(compilerPlugin("org.scalamacros" % "paradise" % paradiseVersion cross CrossVersion.full))
+    else
+      Nil
+    ),
+
+  version in PB.protobufConfig := protobufVersion,
+  PB.runProtoc in PB.protobufConfig := (args =>
+    com.github.os72.protocjar.Protoc.runProtoc(protocVersion  +: args.toArray)
+  ),
+
+  // Release settings
+  releaseCrossBuild             := true,
+  releasePublishArtifactsAction := PgpKeys.publishSigned.value,
+  publishMavenStyle             := true,
+  publishArtifact in Test       := false,
+  sonatypeProfileName           := "me.lyh",
+  pomExtra                      := {
+    <url>https://github.com/spotify/ratatool</url>
+    <licenses>
+      <license>
+        <name>Apache 2</name>
+        <url>http://www.apache.org/licenses/LICENSE-2.0.txt</url>
+      </license>
+    </licenses>
+    <scm>
+      <url>git@github.com/nevillelyh/protobuf-generic.git</url>
+      <connection>scm:git:git@github.com:nevillelyh/protobuf-generic.git</connection>
+    </scm>
+    <developers>
+      <developer>
+        <id>sinisa_lyh</id>
+        <name>Neville Li</name>
+        <url>https://twitter.com/sinisa_lyh</url>
+      </developer>
+    </developers>
+  }
 )
 
-libraryDependencies ++= (
-  if (scalaBinaryVersion.value == "2.10")
-    Seq(compilerPlugin("org.scalamacros" % "paradise" % paradiseVersion cross CrossVersion.full))
-  else
-    Nil
-  )
-
-PB.protobufSettings
-version in PB.protobufConfig := protobufVersion
-PB.runProtoc in PB.protobufConfig := (args =>
-  com.github.os72.protocjar.Protoc.runProtoc(protocVersion  +: args.toArray)
+val noPublishSettings = Seq(
+  publish := {},
+  publishLocal := {},
+  publishArtifact := false
 )
 
-// Release settings
-releaseCrossBuild             := true
-releasePublishArtifactsAction := PgpKeys.publishSigned.value
-publishMavenStyle             := true
-publishArtifact in Test       := false
-sonatypeProfileName           := "me.lyh"
-pomExtra                      := {
-  <url>https://github.com/spotify/ratatool</url>
-  <licenses>
-    <license>
-      <name>Apache 2</name>
-      <url>http://www.apache.org/licenses/LICENSE-2.0.txt</url>
-    </license>
-  </licenses>
-  <scm>
-    <url>git@github.com/nevillelyh/protobuf-generic.git</url>
-    <connection>scm:git:git@github.com:nevillelyh/protobuf-generic.git</connection>
-  </scm>
-  <developers>
-    <developer>
-      <id>sinisa_lyh</id>
-      <name>Neville Li</name>
-      <url>https://twitter.com/sinisa_lyh</url>
-    </developer>
-  </developers>
-}
+lazy val root: Project = Project(
+  "root",
+  file(".")
+).settings(
+  commonSettings ++ noPublishSettings
+).aggregate(
+  core,
+  proto3Test
+)
+
+lazy val core: Project = Project(
+  "protobuf-generic",
+  file("core")
+).settings(
+  commonSettings,
+  name := "protobuf-generic",
+  description := "Generic protobuf manipulation"
+)
+
+lazy val proto3Test: Project = Project(
+  "proto3test",
+  file("proto3test")
+).settings(
+  commonSettings ++ noPublishSettings
+).dependsOn(
+  core
+)
---
+++ proto3test/src/test/scala/me/lyh/protobuf/generic/test/Records.scala
@@ -0,0 +1,131 @@
+package me.lyh.protobuf.generic.test
+
+import com.google.protobuf.ByteString
+import me.lyh.protobuf.generic.proto.Schemas._
+
+import scala.collection.JavaConverters._
+
+object Records {
+
+  def jList[T](xs: T*): java.util.List[T] = xs.asJava
+
+  val optional = Optional.newBuilder()
+    .setDoubleField(math.Pi)
+    .setFloatField(math.E.toFloat)
+    .setInt32Field(10)
+    .setInt64Field(15)
+    .setUint32Field(20)
+    .setUint64Field(25)
+    .setSint32Field(30)
+    .setSint64Field(35)
+    .setFixed32Field(40)
+    .setFixed64Field(45)
+    .setSfixed32Field(50)
+    .setSfixed64Field(55)
+    .setBoolField(true)
+    .setStringField("hello")
+    .setBytesField(ByteString.copyFromUtf8("world"))
+    .setColorField(Color.BLACK)
+    .build()
+
+  val optionalEmpty = Optional.getDefaultInstance
+
+  val repeated = Repeated.newBuilder()
+    .addAllDoubleField(jList(math.Pi, -math.Pi))
+    .addAllFloatField(jList(math.E.toFloat, -math.E.toFloat))
+    .addAllInt32Field(jList(10, 11))
+    .addAllInt64Field(jList(15L, 16L))
+    .addAllUint32Field(jList(20, 21))
+    .addAllUint64Field(jList(25L, 26L))
+    .addAllSint32Field(jList(30, 31))
+    .addAllSint64Field(jList(35L, 36L))
+    .addAllFixed32Field(jList(40, 41))
+    .addAllFixed64Field(jList(45L, 46L))
+    .addAllSfixed32Field(jList(50, 51))
+    .addAllSfixed64Field(jList(55L, 56L))
+    .addAllBoolField(jList(true, false))
+    .addAllStringField(jList("hello", "world"))
+    .addAllBytesField(jList(ByteString.copyFromUtf8("hello"), ByteString.copyFromUtf8("world")))
+    .addAllColorField(jList(Color.BLACK, Color.WHITE))
+    .build()
+
+  val repeatedEmpty = Repeated.getDefaultInstance
+
+  val repeatedPacked = RepeatedPacked.newBuilder()
+    .addAllDoubleField(jList(math.Pi, -math.Pi))
+    .addAllFloatField(jList(math.E.toFloat, -math.E.toFloat))
+    .addAllInt32Field(jList(10, 11))
+    .addAllInt64Field(jList(15L, 16L))
+    .addAllUint32Field(jList(20, 21))
+    .addAllUint64Field(jList(25L, 26L))
+    .addAllSint32Field(jList(30, 31))
+    .addAllSint64Field(jList(35L, 36L))
+    .addAllFixed32Field(jList(40, 41))
+    .addAllFixed64Field(jList(45L, 46L))
+    .addAllSfixed32Field(jList(50, 51))
+    .addAllSfixed64Field(jList(55L, 36L))
+    .addAllBoolField(jList(true, false))
+    .addAllColorField(jList(Color.BLACK, Color.WHITE))
+    .build()
+
+  val repeatedUnpacked = RepeatedUnpacked.newBuilder()
+    .addAllDoubleField(jList(math.Pi, -math.Pi))
+    .addAllFloatField(jList(math.E.toFloat, -math.E.toFloat))
+    .addAllInt32Field(jList(10, 11))
+    .addAllInt64Field(jList(15L, 16L))
+    .addAllUint32Field(jList(20, 21))
+    .addAllUint64Field(jList(25L, 26L))
+    .addAllSint32Field(jList(30, 31))
+    .addAllSint64Field(jList(35L, 36L))
+    .addAllFixed32Field(jList(40, 41))
+    .addAllFixed64Field(jList(45L, 46L))
+    .addAllSfixed32Field(jList(50, 51))
+    .addAllSfixed64Field(jList(55L, 36L))
+    .addAllBoolField(jList(true, false))
+    .addAllColorField(jList(Color.BLACK, Color.WHITE))
+    .build()
+
+  val oneOfs = Array(
+    OneOf.getDefaultInstance,
+    OneOf.newBuilder().setDoubleField(math.Pi).build(),
+    OneOf.newBuilder().setFloatField(math.E.toFloat).build(),
+    OneOf.newBuilder().setInt32Field(10).build(),
+    OneOf.newBuilder().setInt64Field(15L).build(),
+    OneOf.newBuilder().setUint32Field(20).build(),
+    OneOf.newBuilder().setUint64Field(25L).build(),
+    OneOf.newBuilder().setSint32Field(30).build(),
+    OneOf.newBuilder().setSint64Field(35L).build(),
+    OneOf.newBuilder().setFixed32Field(40).build(),
+    OneOf.newBuilder().setFixed64Field(45L).build(),
+    OneOf.newBuilder().setSfixed32Field(50).build(),
+    OneOf.newBuilder().setSfixed64Field(55L).build(),
+    OneOf.newBuilder().setBoolField(true).build(),
+    OneOf.newBuilder().setStringField("hello").build(),
+    OneOf.newBuilder().setBytesField(ByteString.copyFromUtf8("world")).build(),
+    OneOf.newBuilder().setColorField(Color.BLACK).build())
+
+  val mixed = Mixed.newBuilder()
+    .setDoubleFieldO(math.Pi)
+    .setStringFieldO("hello")
+    .setBytesFieldO(ByteString.copyFromUtf8("world"))
+    .setColorFieldO(Color.BLACK)
+    .addAllDoubleFieldR(jList(math.Pi, -math.Pi))
+    .addAllStringFieldR(jList("hello", "world"))
+    .addAllBytesFieldR(jList(ByteString.copyFromUtf8("hello"), ByteString.copyFromUtf8("world")))
+    .addAllColorFieldR(jList(Color.BLACK, Color.WHITE))
+    .build()
+
+  val mixedEmpty = Mixed.getDefaultInstance
+
+  val nested = Nested.newBuilder()
+    .setDoubleFieldO(math.Pi)
+    .addAllDoubleFieldR(jList(math.Pi, -math.Pi))
+    .setColorFieldO(Color.BLACK)
+    .addAllColorFieldR(jList(Color.BLACK, Color.WHITE))
+    .setMixedFieldO(mixed)
+    .addAllMixedFieldR(jList(mixed, mixedEmpty))
+    .build()
+
+  val nestedEmpty = Nested.getDefaultInstance
+
+}
