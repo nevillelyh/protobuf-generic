249063528f328d3a99565b293b236a1e8d49b98f
Neville Li <neville.lyh@gmail.com>
2021-06-04 12:59:13 -0400

    add back protobuf 2 support

--- protoc.sh
+++ protoc.sh
@@ -31,16 +31,31 @@ if [ "$OS" == "osx" ] && [ "$ARCH" == "arm64" ]; then
     ARCH=x86_64
 fi
 
-PREFIX="https://github.com/protocolbuffers/protobuf/releases/download"
-ZIP="protoc-$VERSION-$OS-$ARCH.zip"
-URL="$PREFIX/v$VERSION/$ZIP"
 PROTOC_DIR="$CACHE/$VERSION"
+ARCHIVES="$CACHE/archives"
+PREFIX="https://github.com/protocolbuffers/protobuf/releases/download"
 
-if [ ! -d "$PROTOC_DIR" ]; then
-    ARCHIVES="$CACHE/archives"
-    mkdir -p "$ARCHIVES"
-    curl -sL "$URL" -o "$ARCHIVES/$ZIP"
-    unzip "$ARCHIVES/$ZIP" -d "$PROTOC_DIR"
+if [[ "$VERSION" = 2\.* ]]; then
+    if [ ! -d "$PROTOC_DIR/protobuf-$VERSION" ]; then
+        mkdir -p "$PROTOC_DIR"
+        BZ2="protobuf-$VERSION.tar.bz2"
+        URL="$PREFIX/v$VERSION/$BZ2"
+        curl -sL "$URL" | tar -xjf - -C "$PROTOC_DIR"
+        cd "$PROTOC_DIR/protobuf-$VERSION"
+        ./configure
+        make
+    fi
+    SRC_DIR="$PROTOC_DIR/protobuf-$VERSION/src"
+    PROTOC="$SRC_DIR/protoc"
+    "$PROTOC" -I"$SRC_DIR" -I. $*
+else
+    if [ ! -d "$PROTOC_DIR" ]; then
+        mkdir -p "$ARCHIVES"
+        ZIP="protoc-$VERSION-$OS-$ARCH.zip"
+        URL="$PREFIX/v$VERSION/$ZIP"
+        curl -sL "$URL" -o "$ARCHIVES/$ZIP"
+        unzip "$ARCHIVES/$ZIP" -d "$PROTOC_DIR"
+    fi
+    PROTOC="$PROTOC_DIR/bin/protoc"
+    "$PROTOC" $*
 fi
-
-"$PROTOC_DIR/bin/protoc" $*
--- protoc_all.sh
+++ protoc_all.sh
@@ -2,6 +2,11 @@
 
 set -euo pipefail
 
+PROTO=2.6.1
+OUT="proto2test/src/test/proto-$PROTO"
+mkdir -p "$OUT"
+./protoc.sh $PROTO --java_out="$OUT" proto2test/src/test/protobuf/*.proto
+
 for PROTO in 3.15.8 3.16.0 3.17.2; do
     OUT="proto2test/src/test/proto-$PROTO"
     mkdir -p "$OUT"
