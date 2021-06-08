ebbd407c44a49e56a387ec3291a34a0f49e4209f
Neville Li <neville.lyh@gmail.com>
2021-06-08 13:08:20 -0400

    Bump protobuf-java to 3.17.3

--- protoc_all.sh
+++ protoc_all.sh
@@ -7,7 +7,7 @@ OUT="proto2test/src/test/proto-$PROTO"
 mkdir -p "$OUT"
 ./protoc.sh $PROTO --java_out="$OUT" proto2test/src/test/protobuf/*.proto
 
-for PROTO in 3.15.8 3.16.0 3.17.2; do
+for PROTO in 3.15.8 3.16.0 3.17.3; do
     OUT="proto2test/src/test/proto-$PROTO"
     mkdir -p "$OUT"
     ./protoc.sh $PROTO --java_out="$OUT" proto2test/src/test/protobuf/*.proto
