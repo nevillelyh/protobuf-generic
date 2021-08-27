84d29b15796a600e73b15d0ad9eb91705c94cb98
Scala Steward <me@scala-steward.org>
2021-08-27 21:51:44 +0200

    Reformat with scalafmt 3.0.1

--- jmh/src/test/scala/me/lyh/protobuf/generic/jmh/ProtoBench.scala
+++ jmh/src/test/scala/me/lyh/protobuf/generic/jmh/ProtoBench.scala
@@ -21,12 +21,15 @@ class ProtoBench {
   private val genericRecord = reader.read(bytes)
   private val jsonRecord = genericRecord.toJson
 
-  private val fieldReader = FieldReader.of(schema, Seq(
-    "mixed_field_o.double_field_o",
-    "mixed_field_o.string_field_o",
-    "mixed_field_o.bytes_field_o",
-    "mixed_field_o.color_field_o"
-  ))
+  private val fieldReader = FieldReader.of(
+    schema,
+    Seq(
+      "mixed_field_o.double_field_o",
+      "mixed_field_o.string_field_o",
+      "mixed_field_o.bytes_field_o",
+      "mixed_field_o.color_field_o"
+    )
+  )
 
   @Benchmark def parse: Schema = Schema.of[Schemas.Nested]
   @Benchmark def schemaToJson: String = schema.toJson
@@ -44,7 +47,8 @@ class ProtoBench {
       r.get("mixed_field_o").asInstanceOf[GenericRecord].get("double_field_o"),
       r.get("mixed_field_o").asInstanceOf[GenericRecord].get("string_field_o"),
       r.get("mixed_field_o").asInstanceOf[GenericRecord].get("bytes_field_o"),
-      r.get("mixed_field_o").asInstanceOf[GenericRecord].get("color_field_o"))
+      r.get("mixed_field_o").asInstanceOf[GenericRecord].get("color_field_o")
+    )
   }
 
   @Benchmark def readFields: Seq[Any] = fieldReader.read(bytes).toSeq
