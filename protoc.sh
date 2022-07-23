892c927a53ca3de06ca640f40c51cf4df95068b0
Neville Li <neville.lyh@gmail.com>
2022-07-23 12:34:50 -0400

    handle 3.21+ version scheme

--- protoc.sh
+++ protoc.sh
@@ -2,62 +2,74 @@
 
 set -euo pipefail
 
+set -x
+
 if [ $# -lt 1 ]; then
-    echo "Usage: protoc.sh VERSION [ARG]..."
+    echo "Usage: protoc.sh version [arg]..."
     exit 1
 fi
 
-VERSION=$1
+version=$1
+re='([1-9][0-9]*)\.([1-9][0-9]*)\.(.*)'
+[[ "$version" =~ $re ]]
+major="${BASH_REMATCH[1]}"
+minor="${BASH_REMATCH[2]}"
+patch="${BASH_REMATCH[3]}"
 shift
 
-OS=$(uname -s)
-case "$OS" in
+os=$(uname -s)
+case "$os" in
     Linux)
-        OS="linux"
-        CACHE="$HOME/.cache/protoc.sh"
+        os="linux"
+        cache="$HOME/.cache/protoc.sh"
         ;;
     Darwin)
-        OS="osx"
-        CACHE="$HOME/Library/Caches/protoc.sh"
+        os="osx"
+        cache="$HOME/Library/Caches/protoc.sh"
         ;;
     *)
-        echo "Unknown operating system: $OS"
+        echo "Unknown operating system: $os"
         exit 1
 esac
 
-ARCH=$(uname -m)
+arch=$(uname -m)
 
-if [ "$OS" == "osx" ] && [ "$ARCH" == "arm64" ]; then
-    ARCH=x86_64
+# Apple silicon support in 3.20+
+if [[ "$os" == "osx" ]] && [[ "$arch" == "arm64" ]] && [[ "$minor" -lt 20 ]]; then
+    arch=x86_64
 fi
 
-PROTOC_DIR="$CACHE/$VERSION"
-ARCHIVES="$CACHE/archives"
-PREFIX="https://github.com/protocolbuffers/protobuf/releases/download"
+protoc_dir="$cache/$version"
+archives="$cache/archives"
+prefix="https://github.com/protocolbuffers/protobuf/releases/download"
 
-if [[ "$VERSION" = 2\.* ]]; then
-    if [ ! -d "$PROTOC_DIR/protobuf-$VERSION" ]; then
-        mkdir -p "$PROTOC_DIR"
-        BZ2="protobuf-$VERSION.tar.bz2"
-        URL="$PREFIX/v$VERSION/$BZ2"
-        curl -sL "$URL" | tar -xjf - -C "$PROTOC_DIR"
+if [[ "$major" = "2" ]]; then
+    if [[ ! -d "$protoc_dir/protobuf-$version" ]]; then
+        mkdir -p "$protoc_dir"
+        BZ2="protobuf-$version.tar.bz2"
+        URL="$prefix/v$version/$BZ2"
+        curl -sL "$URL" | tar -xjf - -C "$protoc_dir"
         CWD=$(pwd)
-        cd "$PROTOC_DIR/protobuf-$VERSION"
+        cd "$protoc_dir/protobuf-$version"
         ./configure
         make
         cd "$CWD"
     fi
-    SRC_DIR="$PROTOC_DIR/protobuf-$VERSION/src"
-    PROTOC="$SRC_DIR/protoc"
-    "$PROTOC" -I"$SRC_DIR" $*
+    SRC_DIR="$protoc_dir/protobuf-$version/src"
+    protoc="$SRC_DIR/protoc"
+    "$protoc" -I"$SRC_DIR" "$@"
 else
-    if [ ! -d "$PROTOC_DIR" ]; then
-        mkdir -p "$ARCHIVES"
-        ZIP="protoc-$VERSION-$OS-$ARCH.zip"
-        URL="$PREFIX/v$VERSION/$ZIP"
-        curl -sL "$URL" -o "$ARCHIVES/$ZIP"
-        unzip "$ARCHIVES/$ZIP" -d "$PROTOC_DIR"
+    if [[ ! -d "$protoc_dir" ]]; then
+        mkdir -p "$archives"
+        # Decoupled major versions from languages in 3.21+
+        if [[ "$major" == 3 ]] && [[ "$minor" -ge 21 ]]; then
+            version="$minor.$patch"
+        fi
+        ZIP="protoc-$version-$os-$arch.zip"
+        URL="$prefix/v$version/$ZIP"
+        curl -sL "$URL" -o "$archives/$ZIP"
+        unzip "$archives/$ZIP" -d "$protoc_dir"
     fi
-    PROTOC="$PROTOC_DIR/bin/protoc"
-    "$PROTOC" $*
+    protoc="$protoc_dir/bin/protoc"
+    "$protoc" "$@"
 fi
